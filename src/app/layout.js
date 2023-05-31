4d6cc3c50516c140dc0fb3f3bbadf0a051a379ff
Rainbowyi <auyi8812@gmail.com>
2023-06-01 08:51:11 +1000

    add env

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,8 +4,11 @@ import { Inter } from 'next/font/google'
 import Providers from './Providers'
 import Navbar from '@/components/Navbar'
 import SearchBox from '@/components/SearchBox'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
+
+
 
 export const metadata = {
   title: 'Movie Online',
@@ -16,6 +19,21 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Script 
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+         strategy="afterInteractive" />
+         <Script 
+         id="google-analytics-script"
+         strategy="afterInteractive"
+         >
+         {`
+         window.dataLayer = window.dataLayer || [];
+         function gtag(){dataLayer.push(arguments);}
+         gtag('js', new Date());
+
+         gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+         `}
+         </Script >
         <Providers >
         <Header />
         <Navbar />
