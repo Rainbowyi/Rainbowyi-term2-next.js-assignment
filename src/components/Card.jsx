e67a0dbecb1b24fcd45041427ed32f48cdeb7bc6
Rainbowyi <auyi8812@gmail.com>
2023-06-02 14:55:05 +1000

    picture

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,22 +5,29 @@ import { FiThumbsUp } from "react-icons/fi";
 export default function Card({ result }) {
   return (
     <div className="cursor-pointer sm:p-3 sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200 group">
+
       <Link href={`/movie/${result.id}`}>
-        <Image
+        <div className="relative h-40"> 
+        <Image 
           src={`https://image.tmdb.org/t/p/original/${
             result.backdrop_path || result.poster_path
           }`}
-          width={500}
-          height={300}
-          className="sm:rounded-t-lg group-hover:opacity-80 transition-opacity duration-200"
-          style={{
-            maxWidth: "100%",
-            height: "300px",
-          }}
+         fill={true}
+         
+        
+          // width={500}
+          // height={300}
+          className="sm:rounded-t-lg group-hover:opacity-80 transition-opacity duration-200 object-cover absolute"
+          // style={{
+          //   maxWidth: "100%",
+          //   height: "300px",
+          // }}
           placeholder="blur"
           blurDataURL="/logo.svg"
           alt="image is not available"
         ></Image>
+  </div>
+
         <div className="p-2">
           <p className="line-clamp-2 text-md">{result.overview}</p>
           <h2 className="truncate text-lg font-bold">
